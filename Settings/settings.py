ad570f4b45c4f347f463b30855c934e19f3e03cb
sandyddeveloper <sandyddeveloper@gmail.com>
2026-06-14 14:10:31 +0530

    corns time changes

--- Settings/settings.py
+++ Settings/settings.py
@@ -244,11 +244,11 @@ from celery.schedules import crontab
 CELERY_BEAT_SCHEDULE = {
     'run-etl-monthly-on-day-1': {
         'task': 'core.tasks.run_etl_pipeline',
-        'schedule': crontab(hour=2, minute=10),
+        'schedule': crontab(hour=14, minute=15),
     },
     'run-adzuna-ingestion-daily-1am': {
         'task': 'core.tasks.run_adzuna_ingestion',
-        'schedule': crontab(hour=2, minute=10),
+        'schedule': crontab(hour=14, minute=15),
     },
 }
 
